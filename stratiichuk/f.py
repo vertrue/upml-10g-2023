34aed0d96c3891f5e164c649ba25afb75996b6f6
siiiaalin <124836780+siiiaalin@users.noreply.github.com>
2023-05-04 07:10:40 +0300

    Add files via upload

---
+++ stratiichuk/f.py
@@ -0,0 +1,7 @@
+def v10 (n,a):
+    flag = False
+    for i in range(n):
+        if a[i] == 0:
+            flag = True
+
+        return flag
---
+++ stratiichuk/main.py
@@ -0,0 +1,11 @@
+from f import v10
+
+n = int(input())
+a = []
+for i in range(n):
+a.append(int(input()))
+
+if v10(n, a):
+    print("є")
+else:
+    print("нема")

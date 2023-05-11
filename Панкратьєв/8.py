eb7ebfccabc1acc9862c4c4ef567176c4ff1dab2
panvolodia <124886102+panvolodia@users.noreply.github.com>
2023-05-11 11:12:37 +0300

    Update 8.py

--- Панкратьєв/8.py
+++ Панкратьєв/8.py
@@ -1,6 +1,6 @@
-from f import func:
-    n = (int(input()))
-    a = [0] * n
-    for i in range(n):
-        a[i] = int(input())
-    func(a, n)
+from f import func
+n = (int(input()))
+a = [0] * n
+for i in range(n):
+    a[i] = int(input())
+func(a, n)

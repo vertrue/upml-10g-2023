d64cdd29e28fd0f6e586eb8dcc50fab0eaae5cf2
panvolodia <124886102+panvolodia@users.noreply.github.com>
2023-05-11 11:05:32 +0300

    Update 8.py

--- Панкратьєв/8.py
+++ Панкратьєв/8.py
@@ -1,9 +1,6 @@
+from f import func
 n = (int(input()))
 a = [0] * n
 for i in range(n):
     a[i] = int(input())
-summa = 0
-for i in range(n):
-    if a[i]<=5:
-        summa+=a[i]
-print(summa)
+func(a, n)

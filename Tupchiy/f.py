c83d47ac1c8bc24336d116a52ecd117c6be224ee
Tupchiy Oleksandra <127024587+simunwyy@users.noreply.github.com>
2023-05-11 11:04:32 +0300

    Update f.py

--- Tupchiy/f.py
+++ Tupchiy/f.py
@@ -1,6 +1,6 @@
-def v11 (n, a)
-zeros = 0
-for i in range(n):
-    if a[i] == 0:
-        zeros += 1
-return zeros
+def v11 (n, a):
+    zeros = 0
+    for i in range(n):
+        if a[i] == 0:
+            zeros += 1
+    return zeros
